cb3c511d10c095d21cff25d74d64df3a499df3f8
Gabriel Fernandes <gabriel.work076@gmail.com>
2022-01-04 11:46:10 -0300

    feat: [classes] add crud operations

--- src/modules/classes/resolvers.js
+++ src/modules/classes/resolvers.js
@@ -1,9 +1,31 @@
-const db = require('../../db');
+const { Class: ClassModel } = require('../../models');
 
 module.exports = {
   Query: {
-    classes() {
-      return db.classes;
+    async findClasses() {
+      return await ClassModel.find();
+    },
+  },
+  Mutation: {
+    async createClass(_, { classSession }) {
+      return await ClassModel.create(classSession);
+    },
+    async updateClass(_, { id, body }) {
+      return await ClassModel.updateOne({ id }, body, {
+        runValidators: true,
+        new: true,
+      });
+    },
+    async deleteClass(_, { id }) {
+      try {
+        const { deletedCount } = await ClassModel.deleteOne({ id });
+        return { status: 'success', message: `rows deleted ${deletedCount}` };
+      } catch (err) {
+        return {
+          status: 'fail',
+          message: `error trying to delete class id:${id}`,
+        };
+      }
     },
   },
 };
