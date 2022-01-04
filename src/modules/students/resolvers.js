5e7c7cac87cb5b3a3616f39a9f089a69f1af36cf
Gabriel Fernandes <gabriel.work076@gmail.com>
2022-01-04 10:57:42 -0300

    feat: [student] add crud operations

--- src/modules/students/resolvers.js
+++ src/modules/students/resolvers.js
@@ -1,16 +1,28 @@
-const db = require('../../db');
+const { Student } = require('../../models');
 
 module.exports = {
   Student: {
     signedTo(student) {
-      return (
-        db.classes.filter((value) => student.signedTo.includes(value.id)) || []
-      );
+      return student.signedTo;
     },
   },
   Query: {
-    students() {
-      return db.students;
+    async students() {
+      return await Student.find().populate('signedTo');
+    },
+  },
+  Mutation: {
+    async createStudent(_, { student }) {
+      return await Student.create(student);
+    },
+    async updateStudent(_, { id, body }) {
+      return await Student.updateOne(id, body, {
+        runValidators: true,
+        new: true,
+      });
+    },
+    async deleteStudent(_, { id }) {
+      return await Student.deleteOne(id);
     },
   },
 };
